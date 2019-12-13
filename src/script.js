67a370ebf66bfdc507c2d41269b4bac59ebccb99
Ben <benjisgarza@gmail.com>
2019-12-13 14:32:35 -0700

    Add variables from document, start on tools function

---
+++ src/script.js
@@ -0,0 +1,16 @@
+let playerOneButton = document.querySelector("#playerOneButton");
+let playerTwoButton = document.querySelector("#playerTwoButton");
+let formTogglePlayerOne = document.querySelector("#form-div-playerOne");
+let formTogglePlayerTwo = document.querySelector("#form-div-playerTwo");
+let startButton = document.querySelector("#start-button");
+let gameBoard = document.querySelector("#game-board");
+const nameStorage = [];
+
+const Tools = (() => {
+  
+  
+  
+  
+  
+  return{ };
+})();
