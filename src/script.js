fcd3618c0978420af83eda82fb81c5adb26e65c5
Ben <benjisgarza@gmail.com>
2019-12-13 14:34:47 -0700

    Add player factory function, reset board, and general info display

--- src/script.js
+++ src/script.js
@@ -7,8 +7,36 @@ let gameBoard = document.querySelector("#game-board");
 const nameStorage = [];
 
 const Tools = (() => {
-  
-  
+  const  toggleDisplay = form => form.classList.toggle("toggle-form");
+  const resetBoard = (board) => {
+    board.positions.forEach((element) => {
+        element.innerText = "-";           
+      })
+  }
+  const  playerName = (e) => {    
+  	var buttonIdValue = e.target.attributes.id.value;	
+  	if(buttonIdValue === "save-button-player-one"){
+  		var nameValue = document.querySelector("#name-input-field-player-one").value;
+      toggleDisplay(formTogglePlayerOne);
+      displayGeneralInfo(`Initializing ${nameValue}'s data.......`);
+      instructions("Player two click button bellow to enter your name");
+      toggleDisplay(playerOneButton);
+      toggleDisplay(playerTwoButton);
+      nameStorage.push(nameValue) ;
+  	}else{
+  		var nameValue = document.querySelector("#name-input-field-player-two").value;
+  		toggleDisplay(formTogglePlayerTwo);
+  		displayGeneralInfo(`Initializing ${nameValue}'s data......`);
+  		toggleDisplay(startButton);
+  		instructions("CLICK START BUTTON TO START GAME")
+  		toggleDisplay(playerTwoButton);
+      nameStorage.push(nameValue)
+    }   
+  }
+  const displayGeneralInfo = (info) => {
+    let displayDiv = document.querySelector("#main-info");
+    displayDiv.innerHTML = info
+  }
   
   
   
