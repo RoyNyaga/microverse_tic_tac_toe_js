74484e777cfeaa5dfd8bd7633db040785f29f37b
Ben <benjisgarza@gmail.com>
2019-12-13 14:49:53 -0700

    Added check for win condition and draw

--- src/script.js
+++ src/script.js
@@ -68,7 +68,7 @@ const TicTacToeGame = () => {
   turn = 0;
   var idValue = event.target.attributes.id.value;
   console.log(idValue); 
-  Tools.resetBaord(board);
+  Tools.resetBoard(board);
 
   if(Tools.isStart(idValue)){
     Tools.toggleDisplay(gameBoard);
@@ -131,8 +131,41 @@ const Board = () => {
       };
     };
   };
- 
-  
+  const isDraw = (turn, board) => {
+    if(turn === 9 && board.checkForWinner() === false){
+      Tools.displayGeneralInfo("It's a Draw, click the restart button for Another round");
+      return true;
+    }else{
+      return false;
+    };
+  };
+  let checkForWinner = function(){
+    let winner = false;
+    const winningCombinations = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,4,8],
+      [2,4,6],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8]
+    ];
+    const positions = this.positions;
+    winningCombinations.forEach((winningCombi) => {
+      const pos0InnerText = positions[winningCombi[0]].innerText;
+      const pos1InnerText = positions[winningCombi[1]].innerText;
+      const pos2InnerText = positions[winningCombi[2]].innerText;
+      const isWinningCombi = pos0InnerText !== "-" &&
+       pos0InnerText === pos1InnerText && pos1InnerText === pos2InnerText;
+       if(isWinningCombi){
+        winner = true;
+        winningCombi.forEach((index) => {
+          positions[index].className += " winner";
+        })
+       }
+    })
+    return winner;
   }
   return { positions, checkForWinner, declareWinner, isDraw }
 }
